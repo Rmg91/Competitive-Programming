6c068d3c8cdf6236cfeb3ddfdfff105eb1465829
Rmg_91 <romerg1691@gmail.com>
2018-03-05 01:38:51 -0500

    brute force codes

---
+++ 08_bruteForce/codes/allPermutations.cpp
@@ -0,0 +1,9 @@
+/*** Generate all permutations of an array ***/
+
+void generatePermutations( int A[ ], int n ){
+	sort( A, A + n );
+	do{
+		for( int i = 0; i < n; ++i ) cout << A[ i ] << " ";
+		cout << endl;
+	} while( next_permutation( A, A + n ) );
+}
