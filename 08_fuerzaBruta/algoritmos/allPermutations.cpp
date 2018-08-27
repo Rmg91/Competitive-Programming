b745391fee82be88eb04c29b0e88709e1e300fe7
Rmg_91 <romerg1691@gmail.com>
2018-08-27 16:54:54 -0500

    reestructurando

--- 08_fuerzaBruta/algoritmos/allPermutations.cpp
+++
@@ -1,9 +0,0 @@
-/*** Generate all permutations of an array ***/
-
-void allPermutations( int A[ ], int n ){
-	sort( A, A + n );
-	do{
-		for( int i = 0; i < n; ++i ) cout << A[ i ] << " ";
-		cout << endl;
-	} while( next_permutation( A, A + n ) );
-}
